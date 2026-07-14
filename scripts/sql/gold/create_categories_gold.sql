c3a98faec98bcf6c92540feb13334e98addf21a7
hanskoetjing <55676123+hanskoetjing@users.noreply.github.com>
2026-07-14 07:45:24 +0000

    add version to table and removes duplicate

--- scripts/sql/gold/create_categories_gold.sql
+++ scripts/sql/gold/create_categories_gold.sql
@@ -1,4 +1,5 @@
 CREATE TABLE gold.categories (
 	category_id varchar(6) NULL,
-	category_name varchar(100) NULL
+	category_name varchar(100) NULL,
+	modified TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
--- scripts/sql/silver/create_categories_silver.sql
+++ scripts/sql/silver/create_categories_silver.sql
@@ -1,6 +1,25 @@
 CREATE TABLE silver.categories (
-	category_id varchar(6) NULL,
-	category_name varchar(100) NULL
+	category_id varchar(6) NOT NULL,
+	category_name varchar(100) NULL,
+	modified TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-ALTER TABLE silver.categories ADD CONSTRAINT categories_pk PRIMARY KEY (category_id);
+ALTER TABLE silver.categories ADD CONSTRAINT categories_pk PRIMARY KEY (category_id, modified);
+
+CREATE OR REPLACE FUNCTION silver.insert_cat_to_silver_func() RETURNS trigger as $insert_cat_to_silver_trig$
+	BEGIN
+		PERFORM 1 FROM silver.categories WHERE category_id = NEW.category_id;
+		IF NOT FOUND THEN 
+			RETURN NEW;
+		ELSE 
+			UPDATE silver.categories SET
+				category_name = NEW.category_name,
+				modified = CURRENT_TIMESTAMP
+			WHERE category_id = NEW.category_id;
+			RETURN NULL;
+		END IF;
+	END;
+$insert_cat_to_silver_trig$ language plpgsql;
+
+CREATE OR REPLACE TRIGGER insert_cat_to_silver_trig BEFORE INSERT ON silver.categories
+    FOR EACH ROW EXECUTE FUNCTION silver.insert_cat_to_silver_func();
