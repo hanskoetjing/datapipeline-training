3041d535d69ab66e8eeac327fcf34226273cc1f2
hanskoetjing <55676123+hanskoetjing@users.noreply.github.com>
2026-07-14 06:59:12 +0000

    add primary key in silver

--- scripts/sql/silver/create_categories_silver.sql
+++ scripts/sql/silver/create_categories_silver.sql
@@ -2,3 +2,5 @@ CREATE TABLE silver.categories (
 	category_id varchar(6) NULL,
 	category_name varchar(100) NULL
 );
+
+ALTER TABLE silver.categories ADD CONSTRAINT categories_pk PRIMARY KEY (category_id);
