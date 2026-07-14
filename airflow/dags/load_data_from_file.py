5ddfe238916fa08fa1a89c23cab537505f6f312e
hanskoetjing <55676123+hanskoetjing@users.noreply.github.com>
2026-07-14 04:31:40 +0000

    add glob to load file names

--- airflow/dags/load_data_from_file.py
+++ airflow/dags/load_data_from_file.py
@@ -1,10 +1,12 @@
-from airflow.decorators import dag
+from airflow.decorators import dag, task
 from airflow.operators.bash import BashOperator
 from datetime import datetime
+from glob import glob
+import pandas as pd
 
 @dag(
     dag_id            = "load_data_from_file",
-    description       = "Load data from CSV file to silver db",
+    description       = "Load data from CSV file to bronze db",
     schedule_interval = "* * * * *",
     start_date        = datetime(2026, 7, 1),
     catchup           = False,
@@ -14,12 +16,34 @@ from datetime import datetime
     }
 )
 def main():
-    load_file = BashOperator(
-        task_id = "cobacobasaja",
-        bash_command = "echo test"
-    )
+    @task
+    def load_file():
+        file_obj_ret = []
+        file_paths = glob('data/raw/*/*/*.csv')
+        for file_obj in file_paths:
+            file_obj_split = file_obj.split('/')
+            file_obj_tmp = {
+                "folder" : file_obj_split[-2],
+                "file" : file_obj_split[-1]
+            }
+            file_obj_ret.append(file_obj_tmp)
+        print(file_obj_ret)
+    
+    @task
+    def db_access():
+        from airflow.providers.postgres.hooks.postgres import PostgresHook
 
-    load_file
+        postgres_hook = PostgresHook("postgres_dwh").get_sqlalchemy_engine()
+
+        with postgres_hook.connect() as conn:
+            df = pd.read_sql(
+                sql = "SELECT * FROM customers",
+                con = conn,
+            )
+        print(df)
+    t1 = load_file()
+    t2 = db_access()
+    t1 >> t2
 
 main()
 
